a5176eea468c68a889a4cea22feb16cb8f93046c
Diaeblo <diaeblo33@gmail.com>
2025-01-21 18:06:20 +0530

    Printing Prime Numbers

---
+++ PrimeNumber.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool Prime(int num){
+    int count = 0;
+
+    for (int i = 1; i*i <= num; i++) {
+        if (num % i == 0) {
+            count++;
+            if (num/i != i) count++;
+        }
+    }
+    if (count == 2) return true;
+    else return false;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number : ";
+    cin >> num;
+
+    bool isPrime = Prime(num);
+
+    if (isPrime == true) cout << "The number " << num << " is prime number.";
+    else cout << "The number " << num << " is not a prime number.";
+
+    return 0;
+}
